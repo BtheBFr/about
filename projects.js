a77864df5fd63d454b2d2460e5d610036b4c0ec1
BtheBFr <rtfmqwertu29v@gmail.com>
2026-02-28 15:21:28 +0500

    Create projects.js

---
+++ projects.js
@@ -0,0 +1,40 @@
+const projectsData = [
+    {
+        title: "B the B<br>музыка",
+        description: "Все площадки, все треки. Слушай где удобно.",
+        link: "https://band.link/btheb",
+        status: "active",
+        statusText: "Активно"
+    },
+    {
+        title: "School Hub",
+        description: "Образовательный хаб. Здесь будет много полезного для учебы.",
+        link: "https://bthebfr.github.io/schoolhub/",
+        status: "wip",
+        statusText: "В разработке"
+    },
+    {
+        title: "Завод Осколки<br>коллаборация",
+        description: "Совместный проект. Готовим что-то особенное.",
+        link: "https://bthebfr.github.io/about/",
+        status: "soon",
+        statusText: "Скоро"
+    }
+];
+
+const announcementsData = [
+    {
+        badge: "news",
+        badgeText: "Новость",
+        text: "Открылся этот сайт-навигатор",
+        date: "Сегодня",
+        icon: "fa-star"
+    },
+    {
+        badge: "upcoming",
+        badgeText: "Предстоящее",
+        text: "Скоро откроется 3 сайт (Завод Осколки)",
+        date: "Март 2026",
+        icon: "fa-hourglass-half"
+    }
+];
